d188f1947941ea533beeed866bb0bedbda3b2f49
Detcher <3232390584@qq.com>
2023-02-09 17:28:56 +0800

    v1.0 records of x86 asmlang's learning

---
+++ x86/src/my_printf.c
@@ -0,0 +1,16 @@
+void myprintf( char *, ... );
+
+main()
+{
+    myprintf( "%d %c adas %%\n", -12345, 48 );
+}
+
+void myprintf( char *pS, ... )
+{
+    
+    while( *pS != 0 ) {
+        if( *pS == '%' ) {
+            
+        }
+    }
+}
